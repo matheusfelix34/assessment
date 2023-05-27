79edd4e5e1ddc17c15e894ba04b1e1d966e8d2a2
Matheus Henrique <mfelix3411@gmail.com>
2023-05-27 10:44:25 -0300

    chore/Set up initial development environment

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Assessment</title>
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Reports</a></li>
+      </ul>
+    </nav>
+  </header>
+  
+  <main>
+    <h1>Welcome to Assessment</h1>
+   
+  </main>
+
+  <script src="script.js"></script>
+</body>
+</html>
